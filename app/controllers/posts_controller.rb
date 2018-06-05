4ba3234bade130ecb52304581d17e526e7473e56
nicknish <nickjnish@gmail.com>
2018-06-05 14:17:22 -0700

    Order posts by descending order

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   before_action :ensure_post_author, only: [:edit, :update, :destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def new
